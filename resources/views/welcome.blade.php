7213e553ef0347f333701ce4430f19ce5ea1f33d
VincenzoZ24 <zitov287@gmail.com>
2023-01-26 15:44:23 +0100

    create migratio e seeder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,6 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="css/app.css">
 
         <!-- Styles -->
         <style>
@@ -20,35 +19,28 @@
                 height: 100vh;
                 margin: 0;
             }
-
             .full-height {
                 height: 100vh;
             }
-
             .flex-center {
                 align-items: center;
                 display: flex;
                 justify-content: center;
             }
-
             .position-ref {
                 position: relative;
             }
-
             .top-right {
                 position: absolute;
                 right: 10px;
                 top: 18px;
             }
-
             .content {
                 text-align: center;
             }
-
             .title {
                 font-size: 84px;
             }
-
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -58,18 +50,17 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
             .m-b-md {
                 margin-bottom: 30px;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height container_page_1">
+        <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links barra_home">
+                <div class="top-right links">
                     @auth
-                        <a href="{{ route('admin.home') }}">Home</a>
+                        <a href="{{ route('admin.dashboard') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
